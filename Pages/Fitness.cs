05bea4515d7d67b31f4d959909c9530123618073
Chelsea Dalton <77593854+W0647385@users.noreply.github.com>
2023-03-25 15:27:22 -0400

    created button and new razor component for theme

---
+++ Pages/Fitness.cs
@@ -0,0 +1,12 @@
+public class Fitness
+{
+    public string? Title { get; set; }
+
+    public decimal Price  { get; set; }
+
+    public string? Year { get; set; }
+
+    public string? FitnessLevel { get; set; }
+
+    public bool IsDone { get; set; }
+}
